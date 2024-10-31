88e7bb3f3c3ddc523f529a965100d515987adbac
Ethan Christman <ec72@hawaii.edu>
2024-10-31 12:35:12 -1000

    done in 16 minutes

---
+++ src/app/documentation/page.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable max-len */
+/** Render a Not Authorized page if the user enters a URL that they don't have authorization for. */
+const DocumentationPage = () => (
+  <main>
+    <h1>
+      Documentation
+    </h1>
+    <p>
+      First off, check the&nbsp;
+      <a href="https://ics-software-engineering.github.io/nextjs-application-template/">nextjs-application-template github.io page.</a>
+    </p>
+    <p>If that does not answer your question, you can post a smart question to the class Discord.</p>
+  </main>
+);
+
+export default DocumentationPage;
